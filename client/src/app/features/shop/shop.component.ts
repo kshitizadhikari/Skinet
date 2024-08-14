136d54b890e0d55a813cea59977b2a1a73a75781
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-14 15:10:01 +0545

    add sort functionality

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -4,13 +4,32 @@ import { MatCard } from '@angular/material/card';
 import { MatDialog } from '@angular/material/dialog';
 import { JsonPipe } from '@angular/common';
 import { ProductItemComponent } from './product-item/product-item.component';
-import { MatButton } from '@angular/material/button';
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
 import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
+import {
+  MatListOption,
+  MatSelectionList,
+  MatSelectionListChange,
+} from '@angular/material/list';
+import { MatButton } from '@angular/material/button';
+
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [MatCard, JsonPipe, ProductItemComponent, MatButton, MatIcon],
+  imports: [
+    MatCard,
+    JsonPipe,
+    ProductItemComponent,
+    MatMenu,
+    MatButton,
+    MatSelectionList,
+    MatListOption,
+    MatMenuTrigger,
+    MatIcon,
+    FormsModule,
+  ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
@@ -18,6 +37,12 @@ export class ShopComponent implements OnInit {
   products: any[] = [];
   selectedBrands: string[] = [];
   selectedTypes: string[] = [];
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low-High', value: 'priceAsc' },
+    { name: 'Price: High-Low', value: 'priceDesc' },
+  ];
+  selectedSort: string = '';
   private shopService: ShopService = inject(ShopService);
   private dialogService = inject(MatDialog);
 
@@ -28,10 +53,23 @@ export class ShopComponent implements OnInit {
   initializeShop() {
     this.shopService.getBrands();
     this.shopService.getTypes();
-    this.shopService.getProducts().subscribe({
-      next: (response) => (this.products = response.data),
-      error: (error) => console.error('Error occurred:', error),
-    });
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.shopService
+      .getProducts(this.selectedBrands, this.selectedTypes, this.selectedSort)
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (error) => console.error('Error occurred:', error),
+      });
+  }
+
+  onSortChange(event: MatSelectionListChange) {
+    const selectedOption = event.options[0];
+
+    if (selectedOption) this.selectedSort = selectedOption.value;
+    this.getProducts();
   }
 
   openFiltersDialog() {
@@ -48,12 +86,7 @@ export class ShopComponent implements OnInit {
         if (result) {
           this.selectedBrands = result.selectedBrands;
           this.selectedTypes = result.selectedTypes;
-          this.shopService
-            .getProducts(this.selectedBrands, this.selectedTypes)
-            .subscribe({
-              next: (response) => (this.products = response.data),
-              error: (error) => console.log(error),
-            });
+          this.getProducts();
         }
       },
     });
