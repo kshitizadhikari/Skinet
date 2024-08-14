6c78e740b7adc4d74791a919b4205c3f11e4da57
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-14 13:04:33 +0545

    add getBrands, getTypes and initializeShop functions

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/shop.service';
 import { MatCard } from '@angular/material/card';
 import { JsonPipe } from '@angular/common';
-import { ProductItemComponent } from "./product-item/product-item.component";
+import { ProductItemComponent } from './product-item/product-item.component';
 @Component({
   selector: 'app-shop',
   standalone: true,
@@ -15,6 +15,12 @@ export class ShopComponent implements OnInit {
   private shopService: ShopService = inject(ShopService);
 
   ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  initializeShop() {
+    this.shopService.getBrands();
+    this.shopService.getTypes();
     this.shopService.getProducts().subscribe({
       next: (response) => (this.products = response.data),
       error: (error) => console.error('Error occurred:', error),
