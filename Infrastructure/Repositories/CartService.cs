e2deb83fa1ccd6062671a737849de081c5012242
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-15 14:12:04 +0545

    Create Cart service

---
+++ Infrastructure/Repositories/CartService.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using IDatabase = StackExchange.Redis.IDatabase;
+
+namespace Infrastructure.Repositories
+{
+    public class CartService(IConnectionMultiplexer redis) : ICartService
+    {
+        private readonly IDatabase _database = redis.GetDatabase();
+       
+        public async Task<bool> DeleteCartAsync(string key)
+        {
+            return await _database.KeyDeleteAsync(key);
+        }
+
+        public async Task<ShoppingCart> GetCartAsync(string key)
+        {
+            var data = await _database.StringGetAsync(key);
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<ShoppingCart>(data!);
+        }
+
+        public async Task<ShoppingCart?> SetCartAsync(ShoppingCart cart)
+        {
+            var created = await _database.StringSetAsync(cart.Id, JsonSerializer.Serialize(cart), TimeSpan.FromDays(10));
+            if (!created) return null;
+            return await GetCartAsync(cart.Id);
+        }
+    }
+}
