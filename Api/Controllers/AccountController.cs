a229281c876cfb61bbaac3344620697b69ba0fb5
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-18 23:27:05 +0545

    add AccountController

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Api.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+    {
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+            return Ok(user);
+        }
+    }
+}
