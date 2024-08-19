d3813c0effed080b7d655023c335eb475c8b81c3
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-19 21:03:28 +0545

    create text-input component for re-usable input

--- client/src/app/features/account/register/register.component.ts
+++ client/src/app/features/account/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { AccountService } from '../../../core/services/account.service';
 import { Router } from '@angular/router';
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { TextInputComponent } from "../../../shared/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
@@ -17,26 +19,33 @@ import { Router } from '@angular/router';
     MatLabel,
     MatInput,
     MatButton,
-  ],
+    TextInputComponent
+],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
+  private snackbar = inject(SnackbarService);
+  validationErrors: string[] = [];
   router = inject(Router);
 
   registerForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    email: [''],
-    password: [''],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
   });
 
   onSubmit() {
     this.accountService.register(this.registerForm.value).subscribe({
       next: (user) => {
+        this.snackbar.success('Registration Successful - You can now login.');
         this.router.navigateByUrl('/account/login');
+      },
+      error: (errors) => {
+        this.validationErrors = errors;
       },
     });
   }
