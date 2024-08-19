5705d7db6f0dbf033d919223e69bc8ba8baf565d
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-19 13:14:25 +0545

    add account service and account components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,11 +6,17 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { ServerErrorComponent } from './features/server-error/server-error.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
+import { RegisterComponent } from './features/account/register/register.component';
+import { LoginComponent } from './features/account/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'account/login', component: LoginComponent },
+  { path: 'account/register', component: RegisterComponent },
   { path: 'shop/:id', component: ProductDetailsComponent },
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
