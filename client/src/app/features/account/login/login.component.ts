c89c9efa743321b790ff31049a04b23872f486ac
kshitiz <adhikarikshitiz12@gmail.com>
2024-08-19 23:13:10 +0545

    use authGuard to protect checkout route

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -4,7 +4,7 @@ import { MatButton } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../../../core/services/account.service';
 
 @Component({
@@ -24,7 +24,14 @@ import { AccountService } from '../../../core/services/account.service';
 export class LoginComponent {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
-  router = inject(Router);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  returnUrl = '/shop';
+
+  constructor() {
+    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    if (url) this.returnUrl = url;
+  }
 
   loginForm = this.fb.group({
     email: [''],
@@ -35,7 +42,7 @@ export class LoginComponent {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
         this.accountService.getUserInfo().subscribe();
-        this.router.navigateByUrl('/shop');
+        this.router.navigateByUrl(this.returnUrl);
       },
     });
   }
